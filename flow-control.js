ce0b55ff16fafc318747ac9f931267f2b6034c34
Parker Preyer <parker.preyer@gmail.com>
2019-10-25 16:13:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,18 +1,20 @@
 function basicTeenager(age) {
   if (age > 13 && age < 19) {
     return "You are a teenager!"
+  }
+}
+
+function teenager(age) {
+  if (age > 13 && age < 19) {
+    return "You are a teenager!"
   } else {
     return "You are not a teenager"
   }
 }
 
-function teenager(age) {
-
-}
-
 function ageChecker(age) {
 
-}
+} 
 
 function ternaryTeenager(age) {
 
