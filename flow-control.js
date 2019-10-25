c1bd2410168914b084374fc128dec5f5e4a2ddf9
Parker Preyer <parker.preyer@gmail.com>
2019-10-25 16:39:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,18 +30,25 @@ function switchAge(age) {
   switch(age) {
     case 13:
       console.log("You are teenager");
+      break;
     case 14:
       console.log("You are teenager");
+      break;
     case 15:
       console.log("You are teenager");
+      break;
     case 16:
       console.log("You are teenager");
+      break;
     case 17:
       console.log("You are teenager");
+      break;
     case 18:
       console.log("You are teenager");
+      break;
     case 19:
       console.log("You are teenager");
+      break;
     default:
       console.log "You have an age");
       break;
